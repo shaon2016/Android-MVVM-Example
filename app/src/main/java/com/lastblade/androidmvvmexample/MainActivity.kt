4763fdc5249f95eaf9670fc94459ed3571897409
shaon2018 <shaondeveloper@gmail.com>
2019-09-12 18:02:08 +0600

    some dependency updated

--- app/src/main/java/com/lastblade/androidmvvmexample/MainActivity.kt
+++ app/src/main/java/com/lastblade/androidmvvmexample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lastblade.androidmvvmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
